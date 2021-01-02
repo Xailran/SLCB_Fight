eb48fcbdb666eb4cebaf88816699ab971dedc27f
Xailran <44332176+Xailran@users.noreply.github.com>
2021-01-02 20:04:00 +1100

    Version 1.0.0

--- Fight_StreamlabsSystem.py
+++ Fight_StreamlabsSystem.py
@@ -8,10 +8,8 @@
 import codecs
 import json
 import os
-import re
 import ctypes
 import winsound
-from time import time
 
 #---------------------------------------
 # Script information
@@ -19,22 +17,13 @@ from time import time
 ScriptName = "Fight"
 Website = "https://www.twitch.tv/Xailran"
 Creator = "Xailran"
-Version = "1.3.3"
+Version = "1.0.0"
 Description = "Let viewers fight each other with a variety of weapons"
 
 #---------------------------------------
 # Versions
 #---------------------------------------
 """
-1.3.3 - Fixed all messages to be customizable
-1.3.2 - Made changes to file handling, so that weapons and description files are no longer reset on an update.
-1.3.1 - Added YouTube compatibility
-1.3.0 - Added Response Reset button.
-1.2.2 - Changed when cooldown starts
-1.2.1 - Mixer-specific and general bug fixes
-1.2.0 - Added $addweapon() parameter.
-1.1.1 - Fixed script to properly work for Mixer and YT. Also fixed bug with using the "accept" settings
-1.1.0 - Added UI options for viewers to select amount of points to fight for, and option for users to "accept" a fight before it actually happens. (Funded by twitch.tv/GodOfRanch)
 1.0.0 - Initial Release
 """
 
@@ -99,20 +88,11 @@ def ResetWeaponsFile():
 							 , u"Reset weapons file?", 4)
 
 	if returnValue == MB_YES:
-		CreateWeapons()
+		location = os.path.join(os.path.dirname(__file__), "Weapons.txt")
+		with codecs.open(location, "w", "utf-8") as f:
+			textline = "apple\r\naxe\r\nboomerang\r\nchainsaw\r\ngolf club\r\npistol\r\nspear\r\nspoon\r\ntaco\r\ntrain\r\n"
+			f.write(textline)
 		MessageBox(0, u"Weapons successfully restored to default values!"
-					, u"Reset complete!", 0)
-
-def ResetResponsesFile():
-	"""Resets responses file back to defaults"""
-	winsound.MessageBeep()
-	returnValue = MessageBox(0, u"You are about to reset the responses file, "
-								"are you sure you want to continue?"
-							 , u"Reset responses file?", 4)
-
-	if returnValue == MB_YES:
-		CreateResponses()
-		MessageBox(0, u"Responses successfully restored to default values!"
 					, u"Reset complete!", 0)
 
 #---------------------------------------
@@ -174,154 +154,74 @@ def Init():
 	MySet = Settings(Parent, settingsFile)
 	global parent
 	parent = Parent
-	global fightDict
-	fightDict = {}
 	
 def Execute(data):
 	"""Required Execute data function"""
-	if not data.IsWhisper() and not data.IsFromDiscord() and data.GetParam(0).lower() == MySet.command.lower():
+	if (not data.IsWhisper() and data.IsFromTwitch() and data.GetParam(0).lower() == MySet.command.lower()):
 		if not HasPermission(data, MySet.FightPermission, MySet.FightPermissionInfo):
 			return
-		if MySet.onlylive and not Parent.IsLive():
+		if (MySet.onlylive and not Parent.IsLive()):
 			message = MySet.respNotLive.format(data.UserName)
 			SendResp(data, message)
 			return
-		if IsOnCooldown(data, "fight"):
+		command = MySet.command.lower()
+		if IsOnCooldown(data, command):
 			Fight(data)
 	
-	if not data.IsWhisper() and not data.IsFromDiscord() and MySet.NeedApproval and data.GetParam(0).lower() == MySet.acceptcommand.lower():
-		global fightDict
-		challenge = False
-		for user, userdict in fightDict.items():
-			if userdict["timestamp"] + MySet.accepttime < int(time()):
-				del fightDict[user]
-			else:
-				if userdict["opponentname"].lower() == data.UserName.lower():
-					if data.IsFromYoutube():
-						userdict["opponentid"] = data.User
-					challenge = True
-					Attack(userdict["data"], userdict)
-					del fightDict[user]
-					break
-		if not challenge:
-			message = MySet.nochallenge.format(data.UserName)
-			SendResp(data, message)
-
 def Tick():
 	"""Required tick function"""
 	return
-
-def Parse(parseString, userid, username, targetid, targetname, message):
-	"""Adds parameters to be used in commands"""
-	#Add Weapon parameter
-	#Group 1 = whole parameter, group 2 = weapon to be added, group 3 = success message, group 4 = failure message
-	aw = re.match('^.*(\$addweapon\("?([^"]+)"?,\s?"?([^"]*)"?,\s?"?([^"]+)"?\)).*', parseString)
-	if aw:
-		with codecs.open(Weapons, encoding="utf-8-sig", mode="r") as file:
-			Item = [line.strip().lower() for line in file]
-		if aw.group(2).lower() in Item or aw.group(2) == "":
-			#Weapon already exists
-			parseString = parseString.replace(aw.group(1), aw.group(4))
-		else:
-			#Weapon needs to be added
-			Item.append(aw.group(2))
-			Item.sort()
-			with codecs.open(Weapons, "w", "utf-8") as f:
-				filedata = Item.pop(0)
-				for x in Item:
-					filedata += "\r\n" + x
-				f.write(filedata)
-			parseString = parseString.replace(aw.group(1), aw.group(3))
-	return parseString
 		
 #---------------------------------------
 # [Optional] Fight functions
 #---------------------------------------
 def Fight(data):
-	"""Full setup for attack function"""
+	"""Start of Fight function"""
 	#Establishes opponent
-	global MySet
-	userfightdict = {"data":data}
-	userfightdict["opponentname"] = data.GetParam(1).replace("@", "")
-	viewerlist = [x for x in Parent.GetViewerList()]
-	if data.IsFromTwitch():
-		viewerlist.append(Parent.GetChannelName())
-	if userfightdict["opponentname"] == "":
+	opponent = data.GetParam(1).replace("@", "")
+	viewerlist = Parent.GetViewerList()
+	if opponent == "":
 		message = MySet.needinfo.format(data.UserName, MySet.command)
 		SendResp(data, message)
 		return
-	if userfightdict["opponentname"].lower() == data.UserName.lower():
+	if opponent.lower() == data.UserName.lower():
 		message = MySet.attackself.format(data.UserName)
 		SendResp(data, message)
 		return
 	check = False
-	if data.IsFromYoutube():
-		check = True
-		MySet.NeedApproval = True
-		userfightdict["opponentname"] = Parent.GetDisplayName(userfightdict["opponentname"].lower())
-		userfightdict["opponentid"] = userfightdict["opponentname"].lower()
-	else:
-		for x in viewerlist:
-			if userfightdict["opponentname"].lower() == Parent.GetDisplayName(x.lower()).lower():
-				userfightdict["opponentname"] = Parent.GetDisplayName(x)
-				userfightdict["opponentid"] = x.lower()
-				check = True
-				break
+	for x in viewerlist:
+		if opponent.lower() == x.lower():
+			opponent = x
+			check = True
+			break
 	if not check:
-		message = MySet.targetoffline.format(data.UserName, userfightdict["opponentname"])
+		message = MySet.targetoffline.format(data.UserName, opponent)
 		SendResp(data, message)
 		return
 
 	#Sets parameters
-	userfightdict["UPoints"] = Parent.GetPoints(data.User)
-	userfightdict["TPoints"] = Parent.GetPoints(userfightdict["opponentid"])
+	Currency = Parent.GetCurrencyName()
+	UPoints = Parent.GetPoints(data.UserName)
+	TPoints = Parent.GetPoints(opponent)
 	#Sets command cost
-	if MySet.UserChoiceSetting == "Standard" or MySet.UserChoiceSetting == "Default" and data.GetParam(2):
-		try:
-			userfightdict["fightpoints"] = int(data.GetParam(2))
-		except:
-			message = MySet.outsiderange.format(data.UserName, str(MySet.FightMinValue), str(MySet.FightMaxValue), MySet.command)
-			SendResp(data,message)
-			return
-		else:
-			if not MySet.FightMinValue <= userfightdict["fightpoints"] <= MySet.FightMaxValue:
-				message = MySet.outsiderange.format(data.UserName, str(MySet.FightMinValue), str(MySet.FightMaxValue), MySet.command)
-				SendResp(data, message)
-				return
+	if MySet.FightSetting == "Constant":
+		FightPoints = MySet.FightCValue
 	else:
-		if MySet.FightSetting == "Constant":
-			userfightdict["fightpoints"] = MySet.FightCValue
-		else:
-			if MySet.FightMinValue < MySet.FightMaxValue:
-				userfightdict["fightpoints"] = Parent.GetRandom(MySet.FightMinValue, MySet.FightMaxValue+1)
-			else: 
-				userfightdict["fightpoints"] = Parent.GetRandom(MySet.FightMaxValue, MySet.FightMinValue+1)
+		if MySet.FightMinValue < MySet.FightMaxValue:
+			FightPoints = Parent.GetRandom(MySet.FightMinValue, MySet.FightMaxValue+1)
+		else: 
+			FightPoints = Parent.GetRandom(MySet.FightMaxValue, MySet.FightMinValue+1)
 	#Checks that all parties have enough points
-	if Parent.GetPoints(data.User) < userfightdict["fightpoints"]:
-		message = MySet.notenough.format(data.UserName, str(userfightdict["fightpoints"]), Parent.GetCurrencyName(), str(userfightdict["UPoints"]))
+	if Parent.GetPoints(data.User) < FightPoints:
+		message = MySet.notenough.format(data.UserName, str(FightPoints), Currency, str(UPoints))
 		SendResp(data, message)
 		return
-	if userfightdict["TPoints"] < userfightdict["fightpoints"]:
-		message = MySet.opponentnotenough.format(userfightdict["opponentname"], str(userfightdict["fightpoints"]), Parent.GetCurrencyName(), str(userfightdict["TPoints"]))
+	if TPoints < FightPoints:
+		message = MySet.opponentnotenough.format(opponent, str(FightPoints), Currency, str(TPoints))
 		SendResp(data, message)
 		return
-	#Approval system
-	Parent.AddCooldown(ScriptName,"fight",MySet.timerCooldown)
-	Parent.AddUserCooldown(ScriptName,"fight",userfightdict["data"].User,MySet.timerUserCooldown)
-	if MySet.NeedApproval:
-		global fightDict
-		userfightdict["timestamp"] = int(time())
-		fightDict[data.UserName] = userfightdict
-		message = MySet.challengeissued.format(data.UserName, userfightdict["opponentname"], MySet.acceptcommand, str(MySet.accepttime))
-		SendResp(data, message)
-	else:
-		Attack(data, userfightdict)
-
-def Attack(data, userfightdict):
-	"""Handles the actual fight component"""
+	opponentpoints = {opponent: FightPoints}
 	#Loads weapons
-	if not os.path.exists(Weapons):
-		CreateWeapons()
 	with codecs.open(Weapons, encoding="utf-8-sig", mode="r") as file:
 		Item = [line.strip() for line in file]
 		UWeapon = Item[Parent.GetRandom(0, len(Item))]
@@ -331,42 +231,31 @@ def Attack(data, userfightdict):
 			OWeapon = Item[Parent.GetRandom(0, len(Item))]
 			count = count + 1
 			if count == 3:
-				message = MySet.identicalweapons
-				SendResp(userfightdict["data"], message)
+				message = "Despite my best efforts, we only seem to have one weapon available!"
+				SendResp(data, message)
 				break
 	#Loads fight message
-	if not os.path.exists(FightDescriptions):
-		CreateResponses()
 	with codecs.open(FightDescriptions, encoding="utf-8-sig", mode="r") as file:
 		Item = [line.strip() for line in file]
 		VictoryMessage = Item[Parent.GetRandom(1, len(Item))]
 	#Determines winner
 	if MySet.FightAttWinChance >= Parent.GetRandom(0, 101):
-		Parent.RemovePoints(userfightdict["opponentid"], userfightdict["opponentname"], userfightdict["fightpoints"])
-		Parent.AddPoints(userfightdict["data"].User,userfightdict["data"].UserName,userfightdict["fightpoints"])
-		message = VictoryMessage.format(userfightdict["data"].UserName, UWeapon, userfightdict["opponentname"], OWeapon)
-		SendResp(userfightdict["data"], message)
-		message = MySet.userwon.format(userfightdict["data"].UserName, userfightdict["opponentname"], userfightdict["fightpoints"], Parent.GetCurrencyName())
-		SendResp(userfightdict["data"], message)
+		Parent.RemovePointsAll(opponentpoints)
+		Parent.AddPoints(data.User,data.UserName,FightPoints)
+		message = VictoryMessage.format(data.UserName, UWeapon, opponent, OWeapon)
+		SendResp(data, message)
+		message = "{0}, you WON {2} {3}. {1}, you LOST {2} {3}".format(data.UserName, opponent, FightPoints, Currency)
+		SendResp(data, message)
 	else:
-		Parent.RemovePoints(userfightdict["data"].User,userfightdict["data"].UserName, userfightdict["fightpoints"])
-		Parent.AddPoints(userfightdict["opponentid"], userfightdict["opponentname"], userfightdict["fightpoints"])
-		message = VictoryMessage.format(userfightdict["opponentname"], OWeapon, userfightdict["data"].UserName, UWeapon)
-		SendResp(userfightdict["data"], message)
-		message = MySet.targetwon.format(userfightdict["data"].UserName, userfightdict["opponentname"], userfightdict["fightpoints"], Parent.GetCurrencyName())
-		SendResp(userfightdict["data"], message)
-
-def CreateWeapons():
-	"""Replaces/creates weapons file with default weapons"""
-	with codecs.open(Weapons, "w", "utf-8") as f:
-		textline = "apple\r\naxe\r\nboomerang\r\nchainsaw\r\ngolf club\r\npistol\r\nspear\r\nspoon\r\ntaco\r\ntrain"
-		f.write(textline)
-
-def CreateResponses():
-	"""Replaces/creates responses file with default responses"""
-	with codecs.open(FightDescriptions, "w", "utf-8") as f:
-		textline = "Formatting: {0} = winner, {1} = winner's weapon, {2} = loser, {3} = loser's weapon\r\n{0} used a(n) {1} to fight {2}, and crushed in their skull!\r\nFailure! Not for you though {0}. Your glorious {1} pulverised {2}'s {3}.\r\nAhhh, victory! {0}'s fantastic {1} smashed {2} with their {3}.\r\nWith a {1}, {0} attacked {2}. Even with their {3}. {2} could do nothing to win this fight!\r\n{2} used a(n) {3} with horrible results! {0} with their {1} obliterated them!\r\nAhhh, victory! Not for you though {2}. Your puny {3} lost against {0}'s {1}.\r\n{0} used a(n) {1} to fight {2} who used a(n) {3}, and {0} won!"
-		f.write(textline)
+		Parent.RemovePoints(data.User,data.UserName, FightPoints)
+		Parent.AddPointsAll(opponentpoints)
+		message = VictoryMessage.format(opponent, OWeapon, data.UserName, UWeapon)
+		SendResp(data, message)
+		message = "{0}, you LOST {2} {3}. {1}, you WON {2} {3}".format(data.UserName, opponent, FightPoints, Currency)
+		SendResp(data, message)
+	command = MySet.command.lower()
+	Parent.AddCooldown(ScriptName,command,MySet.timerCooldown)
+	Parent.AddUserCooldown(ScriptName,command,data.User,MySet.timerUserCooldown)
 
 #---------------------------------------
 # Classes
@@ -393,26 +282,16 @@ class Settings:
 			self.FightPermission = "Everyone"
 			self.FightPermissionInfo = ""
 			self.FightAttWinChance = 50
-			self.UserChoiceSetting = "Default"
-			self.FightSetting = "Constant"
+			self.FightSetting = "Random"
 			self.FightCValue = 250
 			self.FightMinValue = 100
 			self.FightMaxValue = 500
-			self.NeedApproval = False
-			self.acceptcommand = "accept"
-			self.accepttime = 180
-			self.challengeissued = "{0} has issued a challenge against {1}; Type {2} in chat within the next {3} seconds to accept!"
-			self.nochallenge = "Sorry {0}, but there are no challenges for you to confirm currently!"
 			self.targetoffline = "Hey, that viewer isn't online right now, fighting them would be unfair!"
 			self.needinfo = "You must choose a target when using the {0} command!"
 			self.attackself = "Trying to fight yourself? That's a little angsty"
 			self.respNotLive = "Sorry {0}, but the stream must be live in order to use that command."
 			self.notenough = "{0} -> you don't have the {1} {2} required to use this command."
-			self.outsiderange = "{0} -> you need to enter a number between {1} and {2} for the amount to fight for! Format: {3} (opponent name) (amount)"
 			self.opponentnotenough = "{0} doesn't have enough {2}!"
-			self.userwon = "{0}, you WON {2} {3}. {1}, you LOST {2} {3}"
-			self.targetwon = "{0}, you LOST {2} {3}. {1}, you WON {2} {3}"
-			self.identicalweapons = "Despite my best efforts, we only seem to have one weapon available!"
 			self.notperm = "{0} -> you don't have permission to use this command. permission is: [{1} / {2}]"
 		
 		self.parent = parent
--- README.txt
+++ README.txt
@@ -11,15 +11,6 @@ Website: https://www.twitch.tv/xailran
 #     Versions      #
 #####################
 
-1.3.3 - Fixed all messages to be customizable
-1.3.2 - Made changes to file handling, so that weapons and description files are no longer reset on an update.
-1.3.1 - Added YouTube compatibility
-1.3.0 - Added Response Reset button.
-1.2.2 - Changed when cooldown starts
-1.2.1 - Mixer-specific and general bug fixes
-1.2.0 - Added $addweapon() parameter.
-1.1.1 - Fixed script to properly work for Mixer and YT. Also fixed bug with using the "accept" settings
-1.1.0 - Added UI options for viewers to select amount of points to fight for, and option for users to "accept" a fight before it actually happens. (Funded by twitch.tv/GodOfRanch)
 1.0.0 - Initial Release
 
 Known bugs:
@@ -33,21 +24,26 @@ Set weapons that can be used in the weapons.txt, and use the FightResponses.txt 
 !fight
 	Like the default Duel minigame, but you choose what weapons viewers will randomly use!
 	"!fight @target" or "!fight target". Enjoy the action!
-	If users can choose how much to fight for (set in the UI), "!fight target value" will be the command to use
 
-#####################
-#    Parameters     #
-#####################
-$addweapon("weapon name", "success message", "Failure message") - Adds "weapon name" to the list of weapons that can be used in a fight
-    Example: $addweapon($msg, "$username has added the weapon $msg", "The weapon $msg could not be added")
+#######################
+#   Default Weapons   #
+#######################
+Just in case you deleted them, but want to restore them:
+apple
+axe
+boomerang
+chainsaw
+golf club
+pistol
+spear
+spoon
+taco
+train
 
 ######################
 #   Future Updates   #
 ######################
-Add stats tracking for fights, and parameters for accessing those stats
-Add leaderboard using above stats
-Add UI option for how much to win
-Add won/lost amount to variables in responses.txt
+None planned, share your ideas with me!
 
 ################################
 #   Other Scripts by Xailran   #
@@ -55,15 +51,16 @@ Add won/lost amount to variables in responses.txt
 Please note that commissioned scripts come with idea and sale protection. These scripts are not included in the list below
 Idea protection means that I will not share any ideas without permission from the client.
 Sale protection means that I will not give away a script for free that someone else has paid for, without permission from the original client.
-Commissioning the script only gives the client the rights to use the script, not to sell or otherwise distribute the script.
 Existing clients are able to request early release versions of public scripts. If you would like to commission a script, send me a message on Discord!
 
 Public Scripts:
 Store (1.5.1) (2.0.0 coming out soon!)
-Fight (1.2.2)
-Xailran's Script Bundle (1.2.2)
+Songlist Exporter (1.0.0)
+Fight (1.0.0)
+The Song Didn't Play (in development)
+Total Points (in planning)
 
-Up to date as of 16/02/2019
+Up to date as of 5/12/2018
 
 #############################################
 #   Donations are never expected, but any   #
